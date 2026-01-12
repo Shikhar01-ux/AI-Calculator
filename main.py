b2b5829bd43ca00b77aa9339e85f47b85c8b28f7
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-12 18:53:32 +0530

    Take Two number from user with error handling

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def show_menu():
     print("3. Multiply")
     print("4. Divide")
     print("5. EXIT")
+
+def get_number():
+    try:
+        num1 = float(input("Enter your first number: "))
+        num2 = float(input("Enter your second number: "))
+        return num1, num2
+    except ValueError:
+        print("Invalid input! please return number only.")
+        return None, None
+     
