ce6249d62b8856a433d10098fec0c839d465cc90
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-12 19:17:16 +0530

    Add some new operation

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def show_menu():
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-    print("5. EXIT")
+    print("5. Power")
+    print("6. EXIT")
 
 def get_number():
     try:
@@ -26,14 +27,16 @@ def calculation (choice, num1, num2):
         if num2 == 0:
             return "Error: Division by zero"
         return num1/num2
+    elif choice == 5:
+        return num1**num2
     else:
         return "invalid operation"
     
 def main():
     while True:
         show_menu()
-        choice = int(input("Enter your choice(1-5): "))
-        if choice == 5:
+        choice = int(input("Enter your choice(1-6): "))
+        if choice == 6:
             print("Exiting calculator. Goodbye!")
             break
 
