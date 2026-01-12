a6b7bc5fd88510af185ec1832967231d4ca9f706
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-12 19:00:49 +0530

    Perform calculation based on user choice

--- main.py
+++ main.py
@@ -14,4 +14,19 @@ def get_number():
     except ValueError:
         print("Invalid input! please return number only.")
         return None, None
+
+def calculation (choice, num1, num2):
+    if choice == 1:
+        return num1+num2
+    elif choice == 2:
+        return num1-num2
+    elif choice == 3:
+        return num1*num2
+    elif choice == 4:
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1/num2
+    else:
+        return "invalid operation"
+    
      
